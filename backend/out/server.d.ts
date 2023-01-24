c3d0f6ec5565800a9553b61c0917a43ae2bb2449
thumbd78492 <thumbd78492@gmail.com>
2023-01-24 11:00:33 +0800

    feat: add keycloak

--- backend/out/server.d.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import * as TE from 'fp-ts/TaskEither';
-import { ServerError } from './type/error';
-import { AppConfig } from './type/config';
-declare const startServer: (appConfig: AppConfig) => TE.TaskEither<ServerError, FastifyInstance>;
-export { startServer };
